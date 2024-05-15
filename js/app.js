3bbe85397bf8f9481a4fb8c47c7cb7e2ec2e9c81
Paul Tuhovsky <tuhovsky@gmail.com>
2024-05-15 20:11:52 +0300

    Added comments to burger menu for mobile devices.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 
+// Слушаем клики по кнопке бургера
 burger.addEventListener('click', () => {
+    // При клике по кнопке бургера меняем значение отображения
     menu.classList.toggle('disp')
 });
